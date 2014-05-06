a406615d63eef1992b16e534a44b2d109d41e587
Corey Floyd <cfloyd@urbn.com>
2014-05-06 14:48:46 -0400

    Adding initial files from Modified SVProgressHud

---
+++ URBNImageHud/URBNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  URBNAppDelegate.h
+//  URBNImageHud
+//
+//  Created by Corey Floyd on 5/6/14.
+//  Copyright (c) 2014 URBN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface URBNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ URBNImageHud/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  URBNImageHud
+//
+//  Created by Corey Floyd on 5/6/14.
+//  Copyright (c) 2014 URBN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "URBNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([URBNAppDelegate class]));
+    }
+}
